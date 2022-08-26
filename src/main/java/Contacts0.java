1b64a9e843088e22b7875dc13cb16223cf09b76c
sylviahe171 <sylviahe171@gmail.com>
2022-08-26 17:51:27 +0800

    updated in the lecture

--- src/main/java/Contacts0.java
+++ src/main/java/Contacts0.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 
 public class Contacts0 {
 
+
+    public static final int MAX = 100;
+
     public static void main(String[] args) {
         final Scanner SCANNER = new Scanner(System.in);
-        System.out.println("|| ===================================================");
-        System.out.println("|| ===================================================");
-        System.out.println("|| Contacts - Version 0.0");
-        System.out.println("|| Welcome to Contacts!");
-        System.out.println("|| ===================================================");
-        String[][] list = new String[100][3];
+
+        showWelcomeMessage();
+
+        String[][] list = new String[MAX][3];
         int count = 0;
         while (true) {
             System.out.print("|| " + "Enter command: ");
@@ -62,7 +63,7 @@ public class Contacts0 {
                             && !person1[2].isEmpty() && person1[2].contains("@") ? person1 : null;
                 }
                 if (decodeResult == null) {
-                    feedback = String.format("Invalid command format: %1$s " + (System.lineSeparator() + "|| ") 
+                    feedback = String.format("Invalid command format: %1$s " + (System.lineSeparator() + "|| ")
                             + "%2$s", "add", String.format("%1$s: %2$s", "add", "Adds a person to contacts.") + (System.lineSeparator() + "|| ")
                             + String.format("\tParameters: %1$s", "NAME "
                             + "p/" + "PHONE_NUMBER "
@@ -120,7 +121,7 @@ public class Contacts0 {
                 System.exit(0);
                 // Fallthrough
             default:
-                feedback = String.format("Invalid command format: %1$s " + (System.lineSeparator() + "|| ") 
+                feedback = String.format("Invalid command format: %1$s " + (System.lineSeparator() + "|| ")
                         + "%2$s", commandType, (String.format("%1$s: %2$s", "add", "Adds a person to contacts.") + (System.lineSeparator() + "|| ")
                         + String.format("\tParameters: %1$s", "NAME "
                         + "p/" + "PHONE_NUMBER "
@@ -144,6 +145,14 @@ public class Contacts0 {
                 System.out.println("|| " + m);
             }
         }
+    }
+
+    private static void showWelcomeMessage() {
+        System.out.println("|| ===================================================");
+        System.out.println("|| ===================================================");
+        System.out.println("|| Contacts - Version 0.0");
+        System.out.println("|| Welcome to Contacts!");
+        System.out.println("|| ===================================================");
     }
 
 }
